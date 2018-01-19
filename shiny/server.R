2e46e5c5ab615bb4315839cabbfa4d5296984a81
Jill Cates <cates.jill@gmail.com>
2018-01-18 16:33:55 -0800

    Add plotly hist, also update server.R to import all appropriate R packages

--- shiny/server.R
+++ shiny/server.R
@@ -1,40 +1,61 @@
 library(shiny)
 library(readr)
 library(leaflet)
+library(dplyr)
+library(ggplot2)
+library(plotly)
 
 nycNoise <- read_csv('../data/data_wrangled.csv')
 
+print(str(nycNoise))
+
 shinyServer(function(input, output) {
   
-  nycData  <- reactive({
-    nycNoise %>% 
-      filter(month_created == input$month,
-             day_created == input$day,
-             year_created == input$year)
+  nycData <- reactive({
+      df <- nycNoise %>% 
+        filter(month_created == input$month,
+               day_created == input$day,
+               year_created == input$year)
   })
-  
+
   output$nycTable <- renderTable({ 
     nycData()
   })
   
-  output$timePlot <- renderPlot({
-    if(is.null(nycData())) {
-      return(NULL)
-    }
-    ggplot(nycData(), aes(hour_created)) + 
-      geom_histogram(fill="red") +
-      xlab("hour") +
-      ylab("frequency") +
-      theme_minimal()
+
+  
+  output$timePlot <- renderPlotly({
+    plot_ly(nycData(), x=~hour_created,
+            type = "histogram",
+            histnorm = "probability")
   })
+  
+  output$event <- renderPrint({
+    d <- event_data("plotly_hover")
+    if (is.null(d)) "Hover on a point!" else d
+  })
+  
+  output$info <- renderText({
+    xy_str <- function(e) {
+      if(is.null(e)) return("NULL\n")
+      paste0("x=", round(e$x, 1), " y=", round(e$y, 1), "\n")
+    }
+    xy_range_str <- function(e) {
+      if(is.null(e)) return("NULL\n")
+      paste0("xmin=", round(e$xmin, 1), " xmax=", round(e$xmax, 1), 
+             " ymin=", round(e$ymin, 1), " ymax=", round(e$ymax, 1))
+    }
+    
+    paste0(
+      "click: ", xy_str(input$plot_click)
+    )
+  })
+  
   output$map <- renderLeaflet({
-    # Use leaflet() here, and only include aspects of the map that
-    # won't need to change dynamically (at least, not unless the
-    # entire map is being torn down and recreated).
     pal <- colorFactor(c("navy", "red", "dark green", "black", "yellow", "orange"), 
                        domain = c("Queens", "Brooklyn", "Staten Island", "Manhattan", "Bronx", "Long Island"))
     leaflet(data = nycData()) %>% addTiles() %>%
       addCircleMarkers(~long, ~lat, label = ~as.character(borough), color = ~pal(borough), fillOpacity = 0.5)
   })
-})
 
+})
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,29 +1,32 @@
 library(shiny)
 library(leaflet)
-# Define UI for application that draws a histogram
+library(plotly)
+
 shinyUI(fluidPage(
   titlePanel("Nightlife-related Noise Complaints in NYC"),
-    sidebarPanel(
-      checkboxGroupInput("borough",
-                         "Select Borough:",
-                         choices=c("Manhattan", "Brooklyn", "Bronx", "Queens", "Staten Island", "All")),
-      sliderInput("month", "Month:",
-                   min = 1, max = 12, value = 1
-       ),
-      sliderInput("day", "Date:",
-                   min = 1, max = 31, value = 1
-       ),
-      sliderInput("year", "Year:",
-                  min = 2016, 
-                  max = 2018, 
-                  value = 2016, 
-                  sep = ""
-      )
+  sidebarPanel(
+    checkboxGroupInput("borough",
+                       "Select Borough:",
+                       choices=c("Manhattan", "Brooklyn", "Bronx", "Queens", "Staten Island")),
+    sliderInput("month", "Month:",
+                min = 1, max = 12, value = 1
     ),
-    mainPanel(
-      h3('MAIN PANEL'),
-      plotOutput("timePlot"),
-      leafletOutput("map")
+    sliderInput("day", "Date:",
+                min = 1, max = 31, value = 1
+    ),
+    sliderInput("year", "Year:",
+                min = 2016, 
+                max = 2018, 
+                value = 2016, 
+                sep = ""
     )
+  ),
+  mainPanel(
+    h3('MAIN PANEL'),
+    plotlyOutput("timePlot"),
+    verbatimTextOutput("info"),
+    verbatimTextOutput("event"),
+    leafletOutput("map")
   )
+)
 )
