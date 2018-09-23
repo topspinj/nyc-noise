3eab86e1501c87211e92777f1b9049cb3d313f40
Jill Cates <cates.jill@gmail.com>
2018-09-23 10:57:18 -0700

    Update styling

--- shiny/ui.R
+++ shiny/ui.R
@@ -10,7 +10,7 @@ shinyUI(fluidPage(
                        "Select Borough:",
                        choices=c("Manhattan", "Brooklyn", "Bronx", "Queens", "Staten Island"),
                        selected=c("Manhattan", "Brooklyn", "Bronx", "Queens", "Staten Island")),
-    dateInput("date", label = h4("Date input"), value = "2016-01-01", min="2016-01-01", max="2018-01-15")
+    dateInput("date", label = h5("Date input"), value = "2016-01-01", min="2016-01-01", max="2018-01-15")
   ),
   mainPanel(
     h4(textOutput("selected")),
